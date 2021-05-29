f4470bfd7db883b742c124783f40fdc14f801b8d
GuiDevloper <guicorrea384@gmail.com>
2021-05-29 01:07:10 -0300

    Add i18n strings & point future CLI

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -1,7 +1,9 @@
 module.exports = {
-  warnQuantity: `Quantity of saved comments is different from the code!`,
+  warnQuantity: "Quantity of saved comments is different from the code!",
   logQuantityDiff: (i18n, src) => {
     return `Saved: ${i18n.length}, In the code: ${src.length}`
   },
-  noI18nFound: `Wait, no i18n data found!`
+  noI18nFound: "Wait, no i18n data found!",
+  noBackupFiles: "No backup files found!",
+  restoredBackup: "Restored backup!"
 }
--- src/locales/pt-BR.js
+++ src/locales/pt-BR.js
@@ -1,7 +1,9 @@
 module.exports = {
-  warnQuantity: `Quantidade de comentários salvos é diferente do código!`,
+  warnQuantity: "Quantidade de comentários salvos é diferente do código!",
   logQuantityDiff: (i18n, src) => {
     return `Salvos: ${i18n.length}, No código: ${src.length}`
   },
-  noI18nFound: `Eita, nenhum dado i18n encontrado!`
+  noI18nFound: "Eita, nenhum dado i18n encontrado!",
+  noBackupFiles: "Nenhum backup encontrado!",
+  restoredBackup: "Backup restaurado!"
 }
